e28116d067725292669024b8141d74002be8806d
Pieter van der Meulen <pieter.vanderMeulen@surfnet.nl>
2015-06-15 17:58:47 +0200

    Add Stepup-tiqr and oath-service-php to GitHub release script

--- github-upload-release.sh
+++ github-upload-release.sh
@@ -15,7 +15,7 @@
 
 CWD=`pwd`
 BASEDIR=`dirname $0`
-COMPONENTS=("Stepup-Middleware" "Stepup-Gateway" "Stepup-SelfService" "Stepup-RA")
+COMPONENTS=("Stepup-Middleware" "Stepup-Gateway" "Stepup-SelfService" "Stepup-RA" "Stepup-tiqr" "oath-service-php")
 
 function error_exit {
     echo "${1}"
