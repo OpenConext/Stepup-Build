34a906a1a09fdb0871e38cb356970d701fa1251a
Pieter van der Meulen <pieter.vandermeulen@surfnet.nl>
2019-08-28 15:16:17 +0200

    Add support for building Stepup-Webauthn component (WIP)
    Add support for php72 to the build vm
    Switch to using nodejs 10.x, don't use 6.x from centos 7
    Make explicit which php version is used

--- stepup-build.sh
+++ stepup-build.sh
@@ -17,7 +17,7 @@
 
 CWD=`pwd`
 BASEDIR=`dirname $0`
-COMPONENTS=("Stepup-Middleware" "Stepup-Gateway" "Stepup-SelfService" "Stepup-RA" "Stepup-tiqr" "oath-service-php")
+COMPONENTS=("Stepup-Middleware" "Stepup-Gateway" "Stepup-SelfService" "Stepup-RA" "Stepup-tiqr" "Stepup-Webauthn" "oath-service-php")
 DEFAULT_BRANCH=develop
 BUILD_ENV=build
 
