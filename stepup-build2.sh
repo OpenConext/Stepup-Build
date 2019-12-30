369dd82fe56e2120428b2c040ebaabfd37fd6757
Pieter van der Meulen <pieter.vandermeulen@surfnet.nl>
2019-12-30 13:04:46 +0100

    Fix: Syntax error because of missing "fi"

--- stepup-build2.sh
+++ stepup-build2.sh
@@ -165,6 +165,7 @@ if [  "${COMPONENT}" = "Stepup-tiqr" ] || [  "${COMPONENT}" = "Stepup-Azure-MFA"
     ${PHP} ${COMPOSER_PATH} encore production
     if [ $? -ne "0" ]; then
         error_exit "encore failed"
+    fi
     #${COMPOSER_PATH} archive --format=tar --file="${OUTPUT_DIR}/${NAME}.tar" --no-interaction
     ${PHP} ${COMPOSER_PATH} archive --dir="${OUTPUT_DIR}" --file="${NAME}" --format=tar --no-interaction
     if [ $? -ne "0" ]; then
