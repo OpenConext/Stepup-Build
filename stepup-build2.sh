110304c327d1347047adfa71543b0c44c4abdae3
Michiel Kodde <mkodde@ibuildings.nl>
2022-02-03 11:15:25 +0100

    Sync stage 2 script with changes from master
    The docker changes where created on `develop` not knowing that develop
    was behind `master` several revisions. This pulls the relevant updates
    from master into the new docker way of life.

--- stepup-build2.sh
+++ stepup-build2.sh
@@ -14,21 +14,48 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-CWD=`pwd`
-COMPONENTS=("Stepup-Middleware" "Stepup-Gateway" "Stepup-SelfService" "Stepup-RA" "Stepup-tiqr" "Stepup-Azure-MFA" "Stepup-Webauthn")
-BUILD_ENV=prod
 
+CWD=`pwd`
+# List of supported components
+COMPONENTS=("Stepup-Middleware" "Stepup-Gateway" "Stepup-SelfService" "Stepup-RA" "Stepup-tiqr" "Stepup-Webauthn" "oath-service-php" "Stepup-Azure-MFA")
+SYMFONY_ENV=prod
+
+# colors for prettyfying build command output
+bold=$(tput bold)
+normal=$(tput sgr0)
+white=$(tput setaf 7)
+gray=$(tput setaf 10)
+red=$(tput setaf 1)
+
+# Print error, return to CWD directory and exit
+# Usage:
+#   error_exit <error message>
 function error_exit {
-    echo "${1}"
+    echo "${red}${1}${normal}${white}"
     if [ -n "${TMP_ARCHIVE_DIR}" -a -d "${TMP_ARCHIVE_DIR}" ]; then
         rm -r "${TMP_ARCHIVE_DIR}"
     fi
     cd ${CWD}
     exit 1
-
 }
 
+# Print the shell do_command with a fake prompt and then execute it
+# Usage:
+#   do_command <the shell command to execute>
+function do_command {
+    echo ""
+    cur_dir=`pwd`
+    # emulate terminal prompt to let command standout
+    echo "${gray}[`whoami`@`hostname` `dirname $cur_dir`]$ ${white}${bold}${1}${normal}"
+    eval '${1}'
+    rv=$?
+    if [ $rv -ne 0 ]; then
+      echo "Command failed with code $rv"
+    fi
+    return $rv
+}
 
+#################
 # Process options
 OUTPUT_DIR=$1
 if [ ! -d ${OUTPUT_DIR} ]; then
@@ -58,119 +85,164 @@ if [ "$found" -ne "1" ]; then
     error_exit "Component must be one of: ${COMPONENTS[*]}"
 fi
 
-cd ${COMPONENT}
-
 # Optional TAG or BRANCH name, not verified
 GIT_TAG_OR_BRANCH=$1
 
-# Make name for archive based on git commit hash and date
-COMMIT_HASH=`git log -1 --pretty="%H"`
-COMMIT_DATE=`git log -1 --pretty="%cd" --date=iso`
-COMMIT_Z_DATE=`php -r "echo gmdate('YmdHis\Z', strtotime('${COMMIT_DATE}'));"`
-NAME=${COMPONENT}-${GIT_HEAD}${GIT_TAG_OR_BRANCH}-${COMMIT_Z_DATE}-${COMMIT_HASH}
-
-
-# Find a composer to use
-COMPOSER_PATH=`which composer.phar`
-if [ -z "${COMPOSER_PATH}" ]; then
-    COMPOSER_PATH=`which composer`
-    if [ -z "${COMPOSER_PATH}" ]; then
-        error_exit "Cannot find composer.phar"
+# Find a composer to use. Try "composer.phar" and "composer"
+COMPOSER=`which composer.phar`
+if [ -z "${COMPOSER}" ]; then
+    COMPOSER=`which composer`
+    if [ -z "${COMPOSER}" ]; then
+        error_exit "Cannot find composer.phar or composer"
     fi
 fi
-COMPOSER_VERSION=`${COMPOSER_PATH} --version`
-echo "Using composer: ${COMPOSER_PATH}"
-echo "Composer version: ${COMPOSER_VERSION}"
-echo "Using symfony env: ${BUILD_ENV}"
 
-export SYMFONY_ENV=${BUILD_ENV}
-export APP_ENV=${BUILD_ENV}
+# Set de default component build requirements to those of release-17
+# Requirements for newer components must be set using component_configuration
+ENCORE=yes # Whether to run yarn encore
+NODE_VERSION=14 # Nodejs version for building is 14, This can be set in the component_info file
+SYMFONY_VERSION=4
 
-${COMPOSER_PATH} install --prefer-dist --ignore-platform-reqs --no-dev --no-interaction --optimize-autoloader
-if [ $? -ne "0" ]; then
-    error_exit "Composer install failed"
+echo "Checking component_info file"
+COMPONENT_INFO_FILE="${COMPONENT}/component_info"
+if [ -f "${COMPONENT_INFO_FILE}" ]; then
+    echo "Processing component_info file"
+
+    # Check component_info file, line by line
+    # If successful, source the file, otherwise abort
+
+    # Regular expression for matching a valid line. The line is read using read -r
+    # so a newline is not present. A valid line is either:
+    # 1. Only whitespace, or
+    # 2. Starts with a # (i.e. is a comment), or
+    # 3. Is a setting <variable name>=<value>. Note that the form is restrictive:
+    #    - Variable name: only uppercase characters and underscore
+    #    - Value: only lowercase characters and digits
+    #    - No comment
+    #
+    # ^                                   Match start of string
+    # (
+    #   ([[:space:]]*)|                   Match whitespace only, or
+    #   (#.*)|                            Match comment line, or
+    #   ([A-Z_]+=[a-z0-9]+[[:space:]]*))  Match VAR=value with optional trailing whitespace
+    # )
+    # $                                   Match end of string
+    valid_line_regex='^(([[:space:]]*)|(#.*)|([A-Z_]+=[a-z0-9]+[[:space:]]*))$'
+    while IFS= read -r line
+    do
+    	echo $line
+        [[ $line =~ $valid_line_regex ]]
+        if [ $? -ne 0 ]; then
+            error_exit "Invalid line in component_info: $line"
+        fi
+    done < "${COMPONENT_INFO_FILE}"
+    source ${COMPONENT_INFO_FILE}
+    if [ $? -ne 0 ]; then
+        error_exit "sourcing component_info file failed"
+    fi
+    echo "Applied component_info"
+else
+    echo "No component_info file found for this project"
+    exit 1
 fi
 
 # Get the NPM, Yarn and such into the environment
 export NVM_DIR="/usr/local/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+NVM_VERSION_STRING=`nvm --version`
+NODE_VERSION_STRING=`node --version`
+
+echo "Using nvm version: ${NVM_VERSION_STRING}"
+echo "Using nodejs version ${NODE_VERSION} (${NODE_VERSION_STRING})"
 
 
-echo "Running yarn install with frozen lockfile"
-yarn install --frozen-lockfile
+# Get the php cli to use
+PHP=`which php`
+if [ -z "${PHP}" ]; then
+    error_exit "php not found"
+fi
+PHP_VERSION_STRING=`${PHP} -r 'echo phpversion();'`
+
+COMPOSER_VERSION_STRING=`${PHP} ${COMPOSER} --version`
+
+# Set working directory to the root of the component
+cd ${COMPONENT}
+
+# Make name for archive based on git commit hash and date
+COMMIT_HASH=`git log -1 --pretty="%H"`
+COMMIT_DATE=`git log -1 --pretty="%cd" --date=iso`
+COMMIT_Z_DATE=`${PHP} -r "echo gmdate('YmdHis\Z', strtotime('${COMMIT_DATE}'));"`
+NAME=${COMPONENT}-${GIT_HEAD}${GIT_TAG_OR_BRANCH}-${COMMIT_Z_DATE}-${COMMIT_HASH}
+
+
+# Print composer install configuration
+echo "Using PHP: ${PHP} (${PHP_VERSION_STRING})"
+echo "Using composer: ${COMPOSER} (${COMPOSER_VERSION_STRING})"
+echo "Targeting Symfony version: ${SYMFONY_VERSION}"
+echo "Using symfony environment: ${SYMFONY_ENV}"
+
+# Validate composer.json and composer.lock
+# Detects when composer.lock is out of date relative to composer.json
+do_command "${PHP} ${COMPOSER} validate"
 if [ $? -ne "0" ]; then
-    error_exit "Yarn install with frozen lockfile failed"
+    error_exit "Composer validate failed"
+fi
+echo "Composer validate done"
+
+# Set Symfony build environment
+do_command "export SYMFONY_ENV=${SYMFONY_ENV}"
+
+# Symfony 4 components are using Flex (https://symfony.com/doc/current/setup/flex.html) which
+# requires APP_ENV=prod during composer install (https://symfony.com/doc/current/deployment.html#c-install-update-your-vendors)
+if [ ${SYMFONY_VERSION} = 4 ]; then
+    # Set APP_ENV=prod for symfony Flex
+    do_command "export APP_ENV=${SYMFONY_ENV}"
+fi
+
+# Composer install
+do_command "${PHP} ${COMPOSER} install --prefer-dist --ignore-platform-reqs --no-dev --no-interaction --optimize-autoloader"
+if [ $? -ne "0" ]; then
+    error_exit "Composer install failed"
+fi
+echo "Composer install done"
+echo ""
+
+# Install frontent assets using yarn encore
+if [ "${ENCORE}" = "yes" ]; then
+    YARN_VERSION_STRING=`yarn --version`
+    if [ $? -ne 0 ]; then
+        echo "Could not get yarn version. Is it installed?"
+    fi
+    echo "Using yarn version: ${YARN_VERSION_STRING}"
+
+    # install yarn
+    do_command "yarn --cache-folder=${HOME}/yarn_cache install"
+    if [ $? -ne "0" ]; then
+        error_exit "yarn install failed"
+    fi
+
+    # run yarn encore
+    do_command "yarn --cache-folder=${HOME}/yarn_cache encore production"
+    if [ $? -ne "0" ]; then
+        error_exit "yarn encore failed"
+    fi
 fi
 
 
-echo "Running Webpack encore"
-yarn encore production
+# Create final archive directly
+do_command "${PHP} ${COMPOSER} archive --dir=${OUTPUT_DIR} --file=${NAME} --format=tar --no-interaction"
 if [ $? -ne "0" ]; then
-    error_exit "Encore production failed"
-fi
-
-
-TMP_ARCHIVE_DIR=`mktemp -d "/tmp/${COMPONENT}.XXXXXXXX"`
-if [ $? -ne "0" ]; then
-    error_exit "Could not create temp dir"
-fi
-
-
-${COMPOSER_PATH} archive --format=tar --dir="${TMP_ARCHIVE_DIR}" --no-interaction
-if [ $? -ne "0" ]; then
-    error_exit "Composer achive failed"
-fi
-
-ARCHIVE_TMP_NAME=`find "${TMP_ARCHIVE_DIR}" -name "*.tar"`
-if [ ! -f ${ARCHIVE_TMP_NAME} ]; then
-    error_exit "Archive not found"
-fi
-
-
-# Untar archive we just created so we can add to it
-# tar archives that are appended to (--append) cause trouble during untar on centos
-
-echo "Unpacking archive"
-
-cd ${TMP_ARCHIVE_DIR}
-if [ $? -ne "0" ]; then
-    error_exit "Could not change to archive dir archive"
-fi
-
-tar -xf "${ARCHIVE_TMP_NAME}"
-if [ $? -ne "0" ]; then
-    error_exit "Untar failed"
-fi
-
-# Add composer.phar
-# console mopa:bootstrap:symlink:less requires it
-echo Adding composer.phar
-cp ${COMPOSER_PATH} ${TMP_ARCHIVE_DIR}/composer.phar
-if [ $? -ne "0" ]; then
-    error_exit "Could not copy composer.phar to archive"
-fi
-
-
-rm ${ARCHIVE_TMP_NAME}
-if [ $? -ne "0" ]; then
-    error_exit "Error removing temporary archive"
+    error_exit "Composer archive failed"
 fi
 
 echo "Creating final archive"
-
-# Output dir is relative to CWD
-tar -cf "${OUTPUT_DIR}/${NAME}.tar" .
-if [ $? -ne "0" ]; then
-    error_exit "Error creating archive"
-fi
-
-bzip2 -9 "${OUTPUT_DIR}/${NAME}.tar"
+# Zip the archive
+do_command "bzip2 -9 ${OUTPUT_DIR}/${NAME}.tar"
 if [ $? -ne "0" ]; then
     rm ${CWD}/${NAME}.tar
     error_exit "bzip2 failed"
 fi
 
-rm -r ${TMP_ARCHIVE_DIR}
 cd ${CWD}
 
 echo "Create checksum file" &&
@@ -189,3 +261,5 @@ echo "Created: ${NAME}.tar.bz2"
 echo "Created: ${NAME}.sha"
 
 echo "End of stage2"
+
+exit 0
