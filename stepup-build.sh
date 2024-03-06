30f6904b6d43ebd50b164165edc3edb2643e2d64
Pieter van der Meulen <pieter.vandermeulen@surf.nl>
2024-03-06 09:58:34 +0100

    Catch docker compose error

--- stepup-build.sh
+++ stepup-build.sh
@@ -188,10 +188,14 @@ if [ "$PHP_VERSION" = "72" ]; then
 fi
 
 if [ "$PHP_VERSION" = "82" ]; then
-	echo "Starting stage2 in the build container when using PHP82"
+	echo "Starting PHP 8.2 container for stage2"
 	# Start the container
 	docker-compose -f ../docker-compose-php82.yml up -d
+	if [ $? -ne "0" ]; then
+    error_exit "Could not start build container"
+  fi
 	# "tmp/build.XXXXXXXX" is 18 characters long
+	echo "Starting stage2 build in the container"
 	docker-compose -f ../docker-compose-php82.yml exec -T build-container bash -c "./stepup-build2.sh ${TMP_ARCHIVE_DIR:(-18)} ${COMPONENT} ${NAME}"
 	if [ $? -ne "0" ]; then
 		error_exit "Stage2 failed"
